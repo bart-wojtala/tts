e17ceb842b19a4ec1447edca9bc25cfda39a0eb2
bart-wojtala <bartlomiej.wojtala@gmail.com>
2020-07-13 19:51:54 +0200

    Format text

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class StreamlabsClient:
             print("I'm disconnected!")
 
         sio.connect('https://sockets.streamlabs.com?token=' + token)
-        print('my sid is', sio.sid)
+        print('My sid is:', sio.sid)
         sio.wait()
 
 token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IjE5NTgyN0IxRDJBRDMxREUzQjJBIiwicmVhZF9vbmx5Ijp0cnVlLCJwcmV2ZW50X21hc3RlciI6dHJ1ZSwidHdpdGNoX2lkIjoiMzk2NTk5NTkifQ.jG1vs8NfnS2T5HTcJ_on9-_HokkiO0ACLYevhgUqnfo"
