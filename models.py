e25b9be781e9c110fa195de88320807550501417
bart-wojtala <bartlomiej.wojtala@gmail.com>
2020-07-15 18:35:28 +0200

    Add Donation class

--- models.py
+++ models.py
@@ -2,3 +2,9 @@ class VoiceMessage:
     def __init__(self, voice, message):
         self.voice = voice
         self.message = message
+
+class Donation:
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+        
