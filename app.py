0a2b8afb09eac5514cafdc73e2501e0232fc7b90
Bartłomiej Wojtala <bartlomiej.wojtala@gmail.com>
2020-07-20 22:12:47 +0200

    Update endpoint mappings and server port

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/status', methods=['GET'])
 def health():
     return "Server OK!"
 
-@app.route('/<message>')
+@app.route('/tts/<message>', methods=['GET'])
 def convert_message(message):
     tts_engine = TextToSpeechEngine(message)
     audio, sampling_rate = tts_engine.generate_audio()
     return jsonify(audio=audio.tolist(), rate=sampling_rate)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=8080)
