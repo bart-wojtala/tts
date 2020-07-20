db31b13c63db6c4c9340fe3cc5e74ffbfd8a6d1c
Bartłomiej Wojtala <bartlomiej.wojtala@gmail.com>
2020-07-20 22:01:02 +0200

    Retrieve tts response from server

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from tts_engine import TextToSpeechEngine
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
@@ -8,7 +9,9 @@ def health():
 
 @app.route('/<message>')
 def convert_message(message):
-    return message
+    tts_engine = TextToSpeechEngine(message)
+    audio, sampling_rate = tts_engine.generate_audio()
+    return jsonify(audio=audio.tolist(), rate=sampling_rate)
 
 if __name__ == '__main__':
     app.run()
