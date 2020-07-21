df9bc09405dbdb8733fd23befc00bf736b6fed2c
Bartłomiej Wojtala <bartlomiej.wojtala@gmail.com>
2020-07-21 15:59:21 +0200

    Change dropout param value to 0

--- tts/hparams.py
+++ tts/hparams.py
@@ -58,8 +58,10 @@ def create_hparams(hparams_string=None, verbose=False):
         prenet_dim=256,
         max_decoder_steps=1000,
         gate_threshold=0.5,
-        p_attention_dropout=0.1,
-        p_decoder_dropout=0.1,
+        # p_attention_dropout=0.1,
+        # p_decoder_dropout=0.1,
+        p_attention_dropout=0,
+        p_decoder_dropout=0,
 
         # Attention parameters
         attention_rnn_dim=1024,
