7e49f34c5b06de5ded800593ffd7caff1acc95a7
Bartłomiej Wojtala <bartlomiej.wojtala@gmail.com>
2020-08-07 17:33:25 +0200

    Handle wrong voice at the start of the message

--- tts_engine.py
+++ tts_engine.py
@@ -56,7 +56,57 @@ class TextToSpeechEngine:
                                 sentence += '.'
                             voice_message = VoiceMessage(voice, sentence)
                             messages_to_generate.append(voice_message)
-            if words[i].endswith(':'):
+            elif i == 0 and words[0].endswith(':'):
+                if i < len(words):
+                    if words[0] in self.available_voices:
+                        voice = words[i]
+                        for j in range(i, len(words)):
+                            word = words[j]
+                            if not word in self.available_voices:
+                                if word.isdigit() and len(word) > 36:
+                                    word = word[0:36]
+                                word = word.replace(',', ', ')
+                                if len(sentence) == 0:
+                                    sentence += word
+                                else:
+                                    sentence += ' ' + word
+                            else:
+                                if sentence[-1] not in sentence_separators:
+                                    sentence += '.'
+                                voice_message = VoiceMessage(voice, sentence)
+                                messages_to_generate.append(voice_message)
+                                break
+                            if j == len(words) - 1:
+                                if sentence[-1] not in sentence_separators:
+                                    sentence += '.'
+                                voice_message = VoiceMessage(voice, sentence)
+                                messages_to_generate.append(voice_message)
+                    else:
+                        voice = self.default_voice
+                        sentence += words[0][:-1] + ' '
+                        for j in range(i + 1, len(words)):
+                            word = words[j]
+                            if not word in self.available_voices:
+                                if word.isdigit() and len(word) > 36:
+                                    word = word[0:36]
+                                word = word.replace(',', ', ')
+                                if len(sentence) == 0:
+                                    sentence += word
+                                else:
+                                    sentence += ' ' + word
+                            else:
+                                if sentence[-1] not in sentence_separators:
+                                    sentence += '.'
+                                voice_message = VoiceMessage(voice, sentence)
+                                messages_to_generate.append(voice_message)
+                                break
+                            if j == len(words) - 1:
+                                if sentence[-1] not in sentence_separators:
+                                    sentence += '.'
+                                voice_message = VoiceMessage(voice, sentence)
+                                messages_to_generate.append(voice_message)
+
+            if i != 0 and words[i].endswith(':'):
                 if words[i] in self.available_voices and i < len(words):
                     voice = words[i]
                     for j in range(i + 1, len(words)):
