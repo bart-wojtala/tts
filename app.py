9f19dd047834c8033ecd95be333985afa016bc4c
Bartłomiej Wojtala <bartlomiej.wojtala@gmail.com>
2020-08-08 11:25:55 +0200

    Add better logging for TTS requests

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def health():
 def convert_message():
     message = request.args.get('message')
     voice = request.args.get('voice')
-    print(voice + ' ' + message)
+    log_message(message, voice)
     # tts_engine = TextToSpeechEngine(message)
     # audio, sampling_rate = tts_engine.generate_audio()
     message_list = []
@@ -25,10 +25,16 @@ def convert_message():
 @app.route('/singletts', methods=['GET'])
 def convert_single_message():
     message = request.args.get('message')
-    print(message)
+    log_message(message)
     tts_engine = TextToSpeechEngine(message, "", "", None)
     audio, sampling_rate = tts_engine.generate_single_audio()
     return jsonify(audio=audio.tolist(), rate=sampling_rate)
+
+def log_message(message, voice=''):
+    if voice:
+        print("[LOG] Requested message: ", voice, message)
+    else:
+        print("[LOG] Requested message: ", message)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=9000)
