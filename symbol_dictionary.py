33d2b887c92e7a7ed5c40a646d59354c0800157e
bart-wojtala <bartlomiej.wojtala@gmail.com>
2021-01-26 23:49:18 +0100

    Refactor code

---
+++ symbol_dictionary.py
@@ -0,0 +1,16 @@
+import json
+import re
+import string
+
+
+class SymbolDictionary:
+    def __init__(self):
+        with open('dictionary_symbols.json') as f:
+            self.symbol_dictionary = json.loads(f.read())
+
+    def is_in_dictionary(self, word):
+        print(word)
+        return word in self.symbol_dictionary
+
+    def replace_symbol(self, word):
+        return self.symbol_dictionary[word]
--- word_dictionary.py
+++ word_dictionary.py
@@ -7,14 +7,9 @@ class WordDictionary:
     def __init__(self):
         with open('dictionary.json') as f:
             self.dictionary = json.loads(f.read())
-        with open('symbol_dictionary.json') as f:
-            self.symbol_dictionary = json.loads(f.read())
 
     def is_in_dictionary(self, word):
         return word.translate(str.maketrans('', '', string.punctuation)) in self.dictionary
-
-    def is_in_symbol_dictionary(self, word):
-        return word.translate(str.maketrans('', '', string.punctuation)) in self.symbol_dictionary
 
     def replace_word(self, word):
         punctuation = re.findall(r'[,.?!]+', word)
@@ -23,12 +18,3 @@ class WordDictionary:
         if punctuation:
             translated_word[-1] += punctuation[0]
         return translated_word
-
-    def replace_symbol(self, symbol):
-        punctuation = re.findall(r'[,.?!]+', symbol)
-        temp_symbol = symbol.translate(
-            str.maketrans('', '', string.punctuation))
-        translated_symbol = self.symbol_dictionary[temp_symbol].split()
-        if punctuation:
-            translated_symbol[-1] += punctuation[0]
-        return translated_symbol
