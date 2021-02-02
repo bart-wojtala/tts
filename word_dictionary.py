b90a40f7a858dc0d11cc0a5062d7e6cbcadb88d7
bart-wojtala <bartlomiej.wojtala@gmail.com>
2021-02-02 23:31:09 +0100

    Replace punctuation regex to avoid replacing words with apostrophe

--- word_dictionary.py
+++ word_dictionary.py
@@ -1,19 +1,19 @@
 import json
 import re
-import string
 
 
 class WordDictionary:
     def __init__(self):
         with open('dictionaries/dictionary_words.json') as f:
             self.dictionary = json.loads(f.read())
+            self.punctuation = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
 
     def is_in_dictionary(self, word):
-        return word.translate(str.maketrans('', '', string.punctuation)) in self.dictionary
+        return word.translate(str.maketrans('', '', self.punctuation)) in self.dictionary
 
     def replace_word(self, word):
         punctuation = re.findall(r'[,.?!]+', word)
-        temp_word = word.translate(str.maketrans('', '', string.punctuation))
+        temp_word = word.translate(str.maketrans('', '', self.punctuation))
         translated_word = self.dictionary[temp_word].split()
         if punctuation:
             translated_word[-1] += punctuation[0]
