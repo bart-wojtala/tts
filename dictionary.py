8b68d23f4e59b34838a3eaf440711880ed3b4bb6
bart3s <bartlomiej.wojtala@gmail.com>
2021-05-02 11:14:50 +0200

    Remove AbbreviationDictionary

--- dictionary.py
+++ dictionary.py
@@ -2,26 +2,6 @@ import json
 import re
 
 
-class AbbreviationDictionary:
-    def __init__(self):
-        with open('dictionaries/dictionary_letters.json') as f:
-            self.dictionary = json.loads(f.read())
-            self.punctuation = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
-
-    def is_abbreviation(self, word):
-        translated_word = word.translate(
-            str.maketrans('', '', self.punctuation))
-        return translated_word.isupper() and translated_word.isalpha()
-
-    def replace_abbreviation(self, word):
-        punctuation = re.findall(r'[,.?!]+', word)
-        temp_word = word.translate(str.maketrans('', '', self.punctuation))
-        translated_word = self.dictionary[temp_word].split()
-        if punctuation:
-            translated_word[-1] += punctuation[0]
-        return translated_word
-
-
 class ContractionsDictionary:
     def __init__(self):
         with open('dictionaries/dictionary_contractions.json') as f:
