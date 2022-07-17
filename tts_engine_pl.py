1b27328f30955c247b36bbf95419936978602188
bart3s <bartlomiej.wojtala@gmail.com>
2022-07-17 22:45:05 +0200

    Initialize configuration

--- tts_engine_pl.py
+++ tts_engine_pl.py
@@ -1,6 +1,19 @@
+import nltk.data
+
+
 class TextToSpeechEnginePL:
     def __init__(self):
-        pass
+        self.speakers = {}
+        with open('filelists/flowtron_speakers.csv') as f:
+            for line in f.readlines():
+                line_split = line.split('|')
+                id = int(line_split[0])
+                name = line_split[1].strip()
+                self.speakers[name] = id
+        self.default_speaker = 'bezi:'
+        self.sentence_separators = ['.', '?', '!', ';']
+        self.sentence_tokenizer = nltk.data.load(
+            'tokenizers/punkt/polish.pickle')
 
     def preprocess_message(self):
         pass
